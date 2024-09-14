980cf89da14673c8148b62c65f3bb7ae4f48e3fb
mayank07feb <makedy07feb@gmail.com>
2024-09-14 10:27:36 +0530

    build

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -507,11 +507,12 @@
     <div>
         <div>
             <!-- Parallax Section -->
-            <div class="relative w-full h-[80vh] min-h-[600px] justify-center items-center overflow-hidden cursor-pointer flex">
+            <div
+                class="relative w-full h-[80vh] min-h-[600px] justify-center items-center overflow-hidden cursor-pointer flex">
                 <div id="mouse-parallax-container"
                     class="relative ltr:left-[5%] rtl:right-[5%] ltr:right-0 rtl:left-0 top-auto bottom-auto flex items-center w-[95%] h-[90%]"
                     style="transition: transform 0.3s cubic-bezier(0.25, 0.1, 0.25, 1);">
-    
+
                     <!-- Dark Title Wrapper -->
                     <div class="absolute inset-0 -z-[3] w-full h-full">
                         <div
@@ -522,7 +523,7 @@
                             </div>
                         </div>
                     </div>
-    
+
                     <!-- Video Section -->
                     <div class="absolute inset-0 -z-[1] w-full md:w-[55vw] justify-start cursor-pointer video-wrapper"
                         style="transition: all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1);">
@@ -534,11 +535,12 @@
                             </div>
                         </div>
                     </div>
-    
+
                     <!-- White Title Wrapper -->
                     <div
                         class="absolute ltr:left-auto rtl:right-auto ltr:right-0 rtl:left-0 inset-y-0 -z-[1] w-full h-full">
-                        <div class="static overflow-hidden w-full md:w-[55vw] justify-end items-center origin-[0%_50%] object-fill">
+                        <div
+                            class="static overflow-hidden w-full md:w-[55vw] justify-end items-center origin-[0%_50%] object-fill">
                             <div class="static flex justify-end items-center w-[80vw] h-[80vh]">
                                 <div class="title text-white text-[40px] md:text-[60px] lg:text-[95px] font-light uppercase whitespace-nowrap static"
                                     style="transition: all 0.5s cubic-bezier(0.25, 0.1, 0.25, 1);">
@@ -551,7 +553,6 @@
             </div>
         </div>
     </div>
-    
 
 
     <!-- COLLECTIONS -->
