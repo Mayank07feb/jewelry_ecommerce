20bee021d80e10dd3d6ffe089bf451d216340ead
mayank07feb <makedy07feb@gmail.com>
2024-09-30 11:24:35 +0530

    change

--- resources/views/frontend/alljewellery.blade.php
+++ resources/views/frontend/alljewellery.blade.php
@@ -197,10 +197,14 @@
                                 </div>
                             </div>
                             <div class="flex space-x-4">
-                                <input type="submit" value="Apply"
-                                    class="bg-[#601042] text-white border border-[#601042] px-4 cursor-pointer hover:bg-white hover:text-[#601042] transition-colors duration-300 ease-in-out">
-                                <a href="{{route('alljewellery', ['material' => $material, 'category' => $category?->id])}}" class="bg-[#601042] text-white border border-[#601042] px-4 cursor-pointer hover:bg-white hover:text-[#601042] transition-colors duration-300 ease-in-out">clear</a>
-
+                                <button type="submit"
+                                    class="px-4 py-2 text-white bg-[#601042] border border-[#601042] hover:bg-white hover:text-[#601042] transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#601042] focus:ring-offset-2">
+                                    Apply
+                                </button>
+                                <a href="{{ route('alljewellery', ['material' => $material, 'category' => $category?->id]) }}"
+                                    class="inline-flex items-center justify-center px-4 py-2 text-white bg-[#601042] border border-[#601042] hover:bg-white hover:text-[#601042] transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#601042] focus:ring-offset-2">
+                                    Clear
+                                </a>
                             </div>
                         </div>
                     </div>
